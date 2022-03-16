160039f39f9a918ef385ee10a900efaa61789d05
arjaywalter-bernardino <abernardino@0MLC-ABernardino-2.local>
2022-03-17 02:41:41 +0800

    add base sceen file

---
+++ src/screens/Base.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+function Base({prop}) {
+  return <></>;
+}
+
+const styles = StyleSheet.create({
+  base: {},
+});
+
+export default Base;
